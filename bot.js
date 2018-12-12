46032b603c1224eed276af6888f3623295686e71
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-12 16:05:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -2975,28 +2975,43 @@ client.on("guildMemberAdd", member => {
       }
       });
 
-client.on('message', msg => {
-  if(msg.content === 'hideall') {
-    msg.guild.channels.forEach(c => {
-      c.overwritePermissions(msg.guild.id, {
-        SEND_MESSAGES: false,
-        READ_MESSAGES: false
-    });
-}); //JakeTOXIC
-    msg.reply('**All Channels Have Been Hided** ✅')
-  }
-});
-client.on('message', msg => {
-  if(msg.content === 'showall') {
-    msg.guild.channels.forEach(c => {
-      c.overwritePermissions(msg.guild.id, {
-        SEND_MESSAGES: true,
-        READ_MESSAGES: true
-    });
-});
-    msg.reply('**All Channels Have Been UnHided** ✅')
-  }
-});
+client.on('message', message => {
+           let args = message.content.split(' ').slice(1);
+    if(message.content.split(' ')[0] == '!color'){
+            const embedd = new Discord.RichEmbed()
+      .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+    .setDescription(`**لا يوجد لون بهذا الأسم ** :x: `)
+    .setColor(`ff0000`)
+ 
+     if(!isNaN(args) && args.length > 0)
+     
+ 
+ if    (!(message.guild.roles.find("name",`${args}`))) return  message.channel.sendEmbed(embedd);
+ 
+ 
+        var a = message.guild.roles.find("name",`${args}`)
+                 if(!a)return;
+ const embed = new Discord.RichEmbed()
+                     
+      .setFooter('Requested by '+message.author.username, message.author.avatarURL)
+    .setDescription(`**Done , تم تغير لونك . :white_check_mark: **`)
+  
+    .setColor(`${a.hexColor}`)
+   message.channel.sendEmbed(embed);
+           if (!args)return;
+ setInterval(function(){})
+                   let count = 0;
+                   let ecount = 0;
+         for(let x = 1; x < 201; x++){
+            
+             message.member.removeRole(message.guild.roles.find("name",`${x}`))
+           
+             }
+                 message.member.addRole(message.guild.roles.find("name",`${args}`));
+         
+             
+     }
+ });
 
 
 
