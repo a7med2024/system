d9ced40c043563e34f75529b787320e19f06327f
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-15 21:31:24 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -39,30 +39,6 @@ const sql = require("sqlite");
 }).catch(console.error)
 })
 
-client.on('message', message => {
-    if(message.channel.type === 'dm') {
-        var guildID = '523297882556596224'; // <=============== ايدي السيرفر حقك
-        if(message.content.includes('discord.gg/')) {
-            var member = client.guilds.find(g => g.id === guildID).members.find(m => m.id === message.author.id);
-            member.ban({ reason: 'ADS In Private.' }).catch();
-        }
-    }
-});
-
-var anti_spam = require("discord-anti-spam");
- 
-antispam(bot, {
-  warnBuffer: 3, //Maximum amount of messages allowed to send in the interval time before getting warned.
-  maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-  interval: 1000, // Amount of time in ms users can send a maximum of the maxBuffer variable before getting banned.
-  warningMessage: "stop spamming or I'll whack your head off.", // Warning message send to the user indicating they are going to fast.
-  banMessage: "has been banned for spamming, anyone else?", // Ban message, always tags the banned user in front of it.
-  maxDuplicatesWarning: 7,// Maximum amount of duplicate messages a user can send in a timespan before getting warned
-  maxDuplicatesBan: 10, // Maximum amount of duplicate messages a user can send in a timespan before getting banned
-  deleteMessagesAfterBanForPastDays: 7 // Delete the spammed messages after banning for the past x days.
-});
-
-
 
 const superagent = require("superagent")
 client.on('message' , async (message) => {
