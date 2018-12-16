8ca83bcec15553a9b0c73f6e0933921bbb15961c
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-16 02:31:23 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -882,7 +882,7 @@ if (message.content.startsWith(prefix + 'help')) { /// This is The DMS Code Send
 『!ct <name> / انشاء شات』
 『!cv <name> / انشاء رووم فويس』
 『!delet <name> / مسح الشات او الرووم فويس』
-『❖!ccolors <number> / ينشا لك الوان مع كم الوان تبي』
+『❖!cc1 <number /ينشا لك 50 لون منظم』
    `,`
         ***__Music orders__***
 **
