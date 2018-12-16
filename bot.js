1cef18c6b20fb350879b18d47915bc1d1709f728
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-16 19:52:08 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -61,7 +61,7 @@ client.on("guildMemberAdd", member => {
         if(member.guild.id === "523297882556596224") { ////////////// Mal , Codes هنا ايدي السيرفر
   const channel = member.guild.channels.find('id', '523303303472349196'); ////////////// Mal , Codes هنا ايدي الروم اللي يحب فيه
 if (!channel) return;
-channel.send(**<@${member.user.id}> Welcome To __هنا اسم سيرفرك__  ** ❤️)  
+channel.send(**<@${member.user.id}> Welcome To Star world server  ** 🕊)  
 }});
 client.on('guildMemberAdd', member=> {
     member.addRole(member.guild.roles.find("name"," Star world "));
