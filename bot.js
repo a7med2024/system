5f4a2c3457c8c87d0623773c7776e2a58a862065
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-17 05:02:43 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -66,6 +66,27 @@ client.on('message', message => {
     }
 });
 
+client.on('message', async message => {
+  if(message.author.bot) return;
+  let prefix = '!';
+
+  let command = message.content.split(" ")[0].slice(prefix.length);
+  let args = message.content.split(" ").slice(1);
+  if(!message.content.toLowerCase().startsWith(prefix)) return;
+
+  if(command == 'dcolors') {
+    if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send(لاتمتلك الصلاحيات لفعل ذلك! ❌);
+    message.channel.send("جاري المسح..").then(async m => {
+      await message.guild.roles.forEach(role => {
+        if(/^\d+$/gi.test(role.name)) {
+          role.delete();
+        }
+      });
+      await m.edit(تم إزالة جميع الالوان.)
+    });
+  }
+});
+
 client.on('message', message => {
 const prefix = '!' 
     if(message.content === prefix + 'cc1') {
