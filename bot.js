1aa8d5e7476da9588036715be07b8a115ad77287
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-18 14:14:35 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,12 +31,7 @@ const sql = require("sqlite");
 ,ti={}  
 ,spee={};
 
-client.on("ready", () => { // كود رينبو
-  function lol() {
-    client.guilds.get('523297882556596224').roles.find("name", "Star World").setColor("RANDOM");
-  };
-  setInterval(lol, 1000);
-});
+
 
 
 client.on("guildMemberAdd", member => {
