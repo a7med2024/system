87282752a85f5aa50323d7ff4951bcd1f866c8fa
a7med2024 <45364118+a7med2024@users.noreply.github.com>
2018-12-22 19:32:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -31,16 +31,6 @@ const sql = require("sqlite");
 ,ti={}  
 ,spee={};
 
-client.on('guildMemberAdd', member => {
-  member.guild.fetchInvites().then(guildInvites => {
-    const ei = invites[member.guild.id];
-    const invite = guildInvites.find(i => ei.get(i.code).uses < i.uses);
-    const inviter = client.users.get(invite.inviter.id);
-    const stewart = member.guild.channels.find("name", "chat");
-     stewart.send(<@${member.user.id}> تمت الدعوه من <@${inviter.id}>);
-   //  stewart.send(<@${member.user.id}> joined using invite code ${invite.code} from <@${inviter.id}>. Invite was used ${invite.uses} times since its creation.);
-  }); 
-});
 
 client.on("ready", () => { // كود رينبو
   function lol() {
